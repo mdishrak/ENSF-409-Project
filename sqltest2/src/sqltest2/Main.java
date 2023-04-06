fd2f9d07da6f8c54edd58779724abcb697f74567
Md Ishrak Mashroor <mdishrak.mashroor@ucalgary.ca>
2023-04-06 13:09:16 -0600

    Update Main.java

--- sqltest2/src/sqltest2/Main.java
+++ sqltest2/src/sqltest2/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("EWR Volunteer Task Scheduler \n - - - - - - - - ");
         System.out.println("Accessing animal and task lists from Database");
-    	SqlMan dao = new SqlMan();
-        dao.readDataBase();
-        TaskMan taskMan = new TaskMan(dao.mergedList());
+    	SqlMan db = new SqlMan();
+        db.readDataBase();
+        TaskMan taskMan = new TaskMan(db.mergedList());
         
         System.out.println("Managing Tasks...");
         taskMan.makeSchedule();
